83e0f3164d67221da7a113116be19623fe8ef2f2
Rob Myers <rob@robmyers.org>
2016-10-20 16:18:29 -0700

    Fix #42 - Need leading disclaimer on the questionnaire page.

--- pub/questionnaire.php
+++ pub/questionnaire.php
@@ -23,6 +23,16 @@ include_once __DIR__ . '/../include/header.php';
 
        <h2>Returning Author's Rights: The CC Termination of Transfer Tool [BETA]</h2>
 
+       <div class="panel panel-default">
+         <div class="panel-body">
+          <p><small>This website and Termination of Transfer Tool are currently in public beta, and are provided to the public solely for the purposes of testing for receiving feedback on functionality and substantive accuracy. All information and functionality is subject to change, and may not be accurate.</p>
+
+             <p>Please note that the content provided on this website and the Termination of Transfer Tool itself are intended solely for educational purposes and do not constitute legal advice. Termination of transfers is a complex area specific to United States law requiring the analysis of many factors. As a result, nothing on this website can be expected to accurately predict whether any given person has a right to terminate a copyright assignment. Only a licensed attorney can properly evaluate the circumstances specific to each copyrighted work and determine whether a transfer of copyright is terminable. If you believe that you may have a right to terminate a transfer of copyright, you should consult an attorney. Neither Authors Alliance nor Creative Commons provide legal advice, and your use of this website and of the Termination of Transfer Tool does not create an attorney-client relationship. In addition, please note that the information on this website might not be current—please see below of the date of our last review.</p>
+
+           Date last reviewed: October 18, 2016</small></p>
+         </div>
+       </div>
+
        <div class="alert alert-danger" role="alert"
             id="no-javascript-alert"><strong>We're sorry!</strong> You&apos;ll need JavaScript enabled to use this tool.</div>
        <script>
