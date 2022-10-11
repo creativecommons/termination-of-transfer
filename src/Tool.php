ca1103b8634e2ffe47ad3b4d42d5e1aaaf311f20
Samuel Onyijne <samuelord1@gmail.com>
2022-10-11 13:48:23 +0100

    added handleShortcode method to Tool class

--- src/Tool.php
+++ src/Tool.php
@@ -9,7 +9,7 @@ class Tool
     /**
      * This will only work in 5.3 or later and WP 3.8 allows 2.4.2+ ... 
      */
-    public function generateHtml()
+    protected function generateHtml()
     {
         $tot_tool_html = <<<'EOD'
             <div class="panel panel-info hidden" id="answers-table">
@@ -52,5 +52,28 @@ class Tool
             </div>
         EOD;
         return $tot_tool_html;
+    }
+
+    /**
+     * 
+     * @param $atts
+     */
+    public function handleShortcode( $atts ){
+        wp_enqueue_script("jquery");
+        wp_enqueue_script( 'tot-values',
+                           plugins_url( 'js/values.js', __FILE__ ) );
+        wp_enqueue_script( 'tot-rules',
+                           plugins_url( 'js/rules.js', __FILE__ ) );
+        wp_enqueue_script( 'tot-validation',
+                           plugins_url( 'js/validation.js', __FILE__ ) );
+        wp_enqueue_script( 'tot-widgets',
+                           plugins_url( 'js/widgets.js', __FILE__ ) );
+        wp_enqueue_script( 'tot-rendering',
+                           plugins_url( 'js/rendering.js', __FILE__ ) );
+        wp_enqueue_script( 'tot-pdf',
+                           plugins_url( 'js/pdf.js', __FILE__ ) );
+        wp_enqueue_script( 'tot-questions',
+                           plugins_url( 'js/questions.js', __FILE__ ) );
+        return $this->generateHtml();
     }
 }
