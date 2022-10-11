61fbe74e05b1b32f2e7b220046ee2dd972b368b1
Samuel Onyijne <samuelord1@gmail.com>
2022-10-11 13:16:09 +0100

    added license and header comments requirements to entry script as per WordPress plugin requirement

---
+++ termination-of-contract.php
@@ -0,0 +1,29 @@
+<?php
+/*
+    Termination of Transfer - tool to help in returning authors rights.
+    Copyright (C) 2016, 2017  Creative Commons Corporation.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+/*
+Plugin Name: Termination of Tranfer Tool
+Plugin URI: https://github.com/creativecommons/termination-of-transfer
+Version: 1.0.0
+Author: Creative Commons Corporation
+Author URI: https://github.com/creativecommons/
+Requires PHP: 7.4.0
+*/
+?>
