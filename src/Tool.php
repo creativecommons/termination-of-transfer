d2d1b218355bfbd7c940c033111cebb9b412b27c
Samuel Onyijne <samuelord1@gmail.com>
2022-10-11 13:41:28 +0100

    added generateHtml method to Tool class

--- src/Tool.php
+++ src/Tool.php
@@ -1,7 +1,56 @@
 <?php
 namespace CreativeCommons_TOT;
 
+/**
+ * Handles HTML generating nd shortcode logics
+ */
 class Tool
 {
-    
+    /**
+     * This will only work in 5.3 or later and WP 3.8 allows 2.4.2+ ... 
+     */
+    public function generateHtml()
+    {
+        $tot_tool_html = <<<'EOD'
+            <div class="panel panel-info hidden" id="answers-table">
+            <div class="panel-heading">
+                <h3 class="panel-title">Your answers</h3>
+            </div>
+            <div class="panel-body">
+                <table class="table table-striped">
+                <tbody id="answers-table-rows">
+                </tbody>
+                </table>
+            </div>
+            </div>
+
+            <div class="panel panel-primary hidden" id="questionnaire-section">
+            <div class="panel-heading">
+            <h3 class="panel-title" id="section-title"></h3>
+            </div>
+            <div class="panel-body" id="question-rendering-area">
+            </div>
+            </div>
+
+            <div id="alert-area">
+            </div>
+
+            <div class="panel hidden" id="result-area">
+            <div class="panel-heading">
+            <h3 class="panel-title" id="result-area-title"></h3>
+            </div>
+            <div class="panel-body" id="result-area-message">
+            </div>
+            </div>
+
+            <div class="hidden" id="question-progress-buttons">
+            <button class="btn btn-default" id="button-question-back">Back</button>
+            <button class="btn btn-default" id="button-question-next">Next</button>
+            <a href="."
+                class="btn btn-default hidden"
+                id="button-restart">Restart</a>
+            </div>
+        EOD;
+        return $tot_tool_html;
+    }
 }
