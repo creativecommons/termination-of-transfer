55c735f907a0b7c599cab8fad0c1aeee3a827fe1
Samuel Onyijne <samuelord1@gmail.com>
2022-10-19 10:48:44 +0100

    updated assets dir to dist

--- assets/js/pdf.js
+++ assets/js/pdf.js
@@ -23,11 +23,9 @@
 
 const TotPdf = {};
 
-TotPdf.url = `${jQuery(
-  "script[src*='/termination-of-transfer/assets/js/pdf.js']",
-)
+TotPdf.url = `${jQuery("script[src*='/termination-of-transfer/dist/js/pdf.js']")
   .attr("src")
-  .replace(/assets\/js\/pdf\.js.*$/, "")}pdf-result.php`;
+  .replace(/dist\/js\/pdf\.js.*$/, "")}pdf-result.php`;
 
 TotPdf.appendProperty = (details, key, value) => {
   const mapping = { key: key, value: value };
